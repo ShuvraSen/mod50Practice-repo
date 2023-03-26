adbd9fcd47751b0ea62819e223b8fe830fa307cd
ShuvraSen <shuvra.sen.it@gmail.com>
2023-03-26 13:17:45 -0500

    nav

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='header '>
+            <img src={logo} alt="" />
+            {/* a*4[href=$]{$} */}
+            <div>
+            <a href="/shop">shop</a>
+            <a href="/order">order</a>
+            <a href="/login">login</a>
+            <a href="/inventory">inventory</a>
+            </div>
+    
+        </div>
+    );
+};
+
+export default Header;
